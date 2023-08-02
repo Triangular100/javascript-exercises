5ec8c55ac49c66fc64d3e96600e89378ac8da974
Triangular100 <139834502+Triangular100@users.noreply.github.com>
2023-08-01 23:09:28 -0500

    Solve 01-07

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, numRepetitions) {
+    if (numRepetitions < 0) {
+        return "ERROR";
+    }
 
+    let result = "";
+    for (let i = 0; i < numRepetitions; i++) {
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
